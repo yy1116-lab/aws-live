65c609763ecde4016ba4ec87f1b00097ceffed3b
yy1116-lab <ngyy-pm24@student.tarc.edu.my>
2025-12-06 12:35:41 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cffymsvq6kqv.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee_db"
+custombucket = "yiyen-bucket"
+customregion = "us-east-1"
+
